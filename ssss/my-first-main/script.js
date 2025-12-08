da56801843c1129abd9451b14e7478034487a70a
vena11651-ops <vena11651@gmail.com>
2025-12-08 18:17:36 +0300

    Add files via upload

---
+++ ssss/my-first-main/script.js
@@ -0,0 +1,26 @@
+// Кнопка с алертом
+/*document.getElementById('magicButton').addEventListener('click', () => {
+    alert(' Кнопка работает!');
+  });*/
+  
+  // Подсвечиваем активный пункт меню при прокрутке
+  const navLinks = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll('section');
+  
+  window.addEventListener('scroll', () => {
+    let current = '';
+  
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 100;
+      if (scrollY >= sectionTop) {
+        current = section.getAttribute('id');
+      }
+    });
+  
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href').includes(current)) {
+        link.classList.add('active');
+      }
+    });
+  });
